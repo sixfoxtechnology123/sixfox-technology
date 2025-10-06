9d3d304641a3cf13b76a2e8544da3bf929108f52
Bimal3726 <bimalmaity8440@gmail.com>
2025-10-06 15:37:36 +0530

    next

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,11 +12,11 @@ export default function About() {
       <h2 className="text-2xl sm:text-3xl font-bold text-deepNavy mt-8 mb-4">Founder & CTO</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <div className="bg-white p-6 rounded-lg shadow-md text-center">
-          <p className="font-bold text-primaryOrange">Neel Palash Mondal & Aloke Chatterjee</p>
+          <p className="font-bold text-primaryOrange">Palas Basu Mandal & Aloke Chatterjee</p>
           <p className="text-darkGray">Founder</p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md text-center">
-          <p className="font-bold text-primaryOrange">Neel Palash Mondal</p>
+          <p className="font-bold text-primaryOrange">Palas Basu Mandal</p>
           <p className="text-darkGray">CTO</p>
         </div>
       </div>
